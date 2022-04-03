8614da0d656348d565aaa524621d01e5adef305a
Katrusya Panchenko <katrusya.pan4enko@gmail.com>
2022-04-03 18:30:02 +0300

    bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import s from './App.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
@@ -7,15 +7,17 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { nanoid } from 'nanoid';
 
-// const initialContacts =
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? initialContacts;
+  });
   const [filter, setFilter] = useState('');
 
   const addContacts = e => {
@@ -38,10 +40,24 @@ function App() {
     };
 
     setContacts(prev => [...prev, newContacts]);
+    if (localStorage.getItem('contacts')) {
+      return localStorage.setItem(
+        'contacts',
+        JSON.stringify([...contacts, newContacts])
+      );
+    }
+    localStorage.setItem(
+      'contacts',
+      JSON.stringify([...initialContacts, newContacts])
+    );
   };
 
   const deleteContact = contactId => {
     setContacts(prev => prev.filter(contact => contact.id !== contactId));
+    localStorage.setItem(
+      'contacts',
+      JSON.stringify(contacts.filter(contact => contact.id !== contactId))
+    );
   };
 
   const changeFilter = e => {
@@ -57,28 +73,6 @@ function App() {
     );
   };
 
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const contactsParsed = JSON.parse(contacts);
-
-  //   if (contactsParsed) {
-  //     this.setState({ contacts: contactsParsed });
-  //   }
-  // }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   const prevContacts = prevState.contacts;
-  //   const nextContacts = this.state.contacts;
-
-  //   if (prevContacts !== nextContacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(nextContacts));
-  //   }
-  //   // console.log(prevState.contacts);
-  //   // console.log(this.state.contacts);
-  // }
-
-  // const { filter } = this.state;
-  // const visibleContact = getVisibleContact();
   return (
     <div>
       <h2 className={s.title}>Phonebook</h2>
@@ -86,7 +80,7 @@ function App() {
       <h3 className={s.title}>Contacts</h3>
       <Filter filter={filter} onChange={changeFilter} />
       <ContactList
-        contacts={getVisibleContact}
+        contacts={getVisibleContact()}
         onDeleteContact={deleteContact}
       />
       <ToastContainer autoClose={3000} theme={'colored'} />
